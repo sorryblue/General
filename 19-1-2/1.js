cc9484e0dee02b8daa49e081e849b932775b92c6
sorryblue <yan764238964@gmail.com>
2019-01-02 23:35:06 +0800

    19-1-2ALert2

---
+++ 19-1-2/1.js
@@ -0,0 +1,7 @@
+/*
+ * @Author: mikey.zhaopeng 
+ * @Date: 2019-01-02 17:55:44 
+ * @Last Modified by:   mikey.zhaopeng 
+ * @Last Modified time: 2019-01-02 17:55:44 
+ */
+console.log(ctrl + alt + i);
